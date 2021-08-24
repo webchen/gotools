0d1f598aefa790cc4d3a10bf56206fa0b84298d8
jacky_chen <altidc@qq.com>
2021-08-24 12:44:27 +0800

    add consul check

--- base/conf/consul.go
+++ base/conf/consul.go
@@ -48,6 +48,11 @@ func ConsulRegister() {
 		return
 	}
 	cnf := baseConfigData["consul"]
+
+	if cnf["register"].(map[string]interface{})["open"].(string) != "1" {
+		return
+	}
+
 	registration := new(consulapi.AgentServiceRegistration)
 	registration.ID = cnf["register"].(map[string]interface{})["id"].(string) + nettool.GetLocalFirstIPStr() // 服务节点的名称
 	registration.Name = cnf["register"].(map[string]interface{})["name"].(string)                            // 服务名称
@@ -55,15 +60,16 @@ func ConsulRegister() {
 	registration.Tags = cnf["register"].(map[string]interface{})["tags"].([]string)                          // tag，可以为空
 	registration.Address = nettool.GetLocalFirstIPStr()                                                      // 服务 IP
 
-	checkPort := int(str.Convert2Int32(cnf["check"].(map[string]interface{})["port"]))
-	registration.Check = &consulapi.AgentServiceCheck{ // 健康检查
-		HTTP:                           fmt.Sprintf("http://%s:%d%s", registration.Address, checkPort, "/"),
-		Timeout:                        cnf["check"].(map[string]interface{})["timeout"].(string),
-		Interval:                       cnf["check"].(map[string]interface{})["interval"].(string),                       // 健康检查间隔
-		DeregisterCriticalServiceAfter: cnf["check"].(map[string]interface{})["deregisterCriticalServiceAfter"].(string), //check失败后30秒删除本服务，注销时间，相当于过期时间
-		// GRPC:     fmt.Sprintf("%v:%v/%v", IP, r.Port, r.Service),// grpc 支持，执行健康检查的地址，service 会传到 Health.Check 函数中
+	if cnf["check"].(map[string]interface{})["open"].(string) == "1" {
+		checkPort := int(str.Convert2Int32(cnf["check"].(map[string]interface{})["port"]))
+		registration.Check = &consulapi.AgentServiceCheck{ // 健康检查
+			HTTP:                           fmt.Sprintf("http://%s:%d%s", registration.Address, checkPort, "/"),
+			Timeout:                        cnf["check"].(map[string]interface{})["timeout"].(string),
+			Interval:                       cnf["check"].(map[string]interface{})["interval"].(string),                       // 健康检查间隔
+			DeregisterCriticalServiceAfter: cnf["check"].(map[string]interface{})["deregisterCriticalServiceAfter"].(string), //check失败后30秒删除本服务，注销时间，相当于过期时间
+			// GRPC:     fmt.Sprintf("%v:%v/%v", IP, r.Port, r.Service),// grpc 支持，执行健康检查的地址，service 会传到 Health.Check 函数中
+		}
 	}
-
 	err := consulClient.Agent().ServiceRegister(registration)
 	if err != nil {
 		log.Println("register server error : ", err)
